5f2a2a9fee163d270669356dade6d21111831464
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2015-10-29 21:01:18 +0300

    Development branch created

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from pip.download import PipSession
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 # To use a consistent encoding
@@ -16,7 +15,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements(here + "/resources/requirements.txt", session=PipSession())
+install_reqs = parse_requirements(here + "/resources/requirements/dev.txt", session=False)
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
@@ -31,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='development',
 
     description='Translation',
     long_description=long_description,
